3e633a8a9b530da037e2eeaad07c9fc64a4f05a6
superadmin <20forfree@gmail.com>
2016-03-09 22:25:33 -0500

    Version 2

--- app/src/main/java/com/hfad/beat/ArminFragment.java
+++ app/src/main/java/com/hfad/beat/ArminFragment.java
@@ -19,6 +19,7 @@ public class ArminFragment extends ListFragment {
                                                                 android.R.layout.simple_list_item_1,
                                                                 getResources().getStringArray(R.array.armin));
         setListAdapter(adapter);
+
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
